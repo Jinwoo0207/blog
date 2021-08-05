90c08033b3776d5217269907b4846445e2db3139
Jinwoo0207 <heypluto@kakao.com>
2021-08-05 09:43:18 +0900

    add react-ridge-state
    d

--- blog-web/src/App.tsx
+++ blog-web/src/App.tsx
@@ -2,16 +2,31 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { HomePage, ArticlePage } from "./pages";
 import { ArticleWritePage } from "./pages/ArticleWritePage";
+import { recentState } from "./plugins/ridge";
 
 function App() {
+  const recent = recentState.useValue();
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/articles/write" component={ArticleWritePage} />
-        <Route path="/articles/:id" component={ArticlePage} />
-        <Route path="/" component={HomePage} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      {/* Header */}
+      <div className="sticky top-0 p-2 bg-white shadow-lg w-full h-14">
+        <div className="w-full max-w-screen-xl mx-auto flex items-end">
+          <h1 className="font-extrabold text-4xl text-blue-700">블로그</h1>
+          <p className="pl-3 text-xl text-gray-500">블로그입니다.</p>
+          <div>
+            <p>최근 방문한 페이지 : {recent?.title}</p>
+          </div>
+        </div>
+      </div>
+
+      <BrowserRouter>
+        <Switch>
+          <Route path="/articles/write" component={ArticleWritePage} />
+          <Route path="/articles/:id" component={ArticlePage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
--- blog-web/src/pages/HomePage.tsx
+++ blog-web/src/pages/HomePage.tsx
@@ -14,31 +14,20 @@ export const HomePage = () => {
   }, []);
 
   return (
-    <>
-      {/* Header */}
-      <div className="sticky top-0 p-2 bg-white shadow-lg w-full h-14">
-        <div className="w-full max-w-screen-xl mx-auto flex items-end">
-          <h1 className="font-extrabold text-4xl text-blue-700">블로그</h1>
-          <p className="pl-3 text-xl text-gray-500">블로그입니다.</p>
-        </div>
+    <div className="max-w-screen-xl mx-auto">
+      <div className="p-4">
+        <button
+          className="bg-indigo-600 text-white rounded-md px-4 py-2"
+          onClick={() => push("articles/write")}
+        >
+          글 작성
+        </button>
       </div>
-
-      {/* Body */}
-      <div className="max-w-screen-xl mx-auto">
-        <div className="p-4">
-          <button
-            className="bg-indigo-600 text-white rounded-md px-4 py-2"
-            onClick={() => push("articles/write")}
-          >
-            글 작성
-          </button>
-        </div>
-        <div className="grid grid-cols-3 gap-2 gap-y-5 gap-x-2 m-2">
-          {articles.map((article) => (
-            <Card key={article.id} article={article} />
-          ))}
-        </div>
+      <div className="grid grid-cols-3 gap-2 gap-y-5 gap-x-2 m-2">
+        {articles.map((article) => (
+          <Card key={article.id} article={article} />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
